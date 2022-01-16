1d5847c82d16abcd68043c41b4547195fcf26d3c
zhuruohuan <zhayanfen@gmail.com>
2022-01-16 13:30:52 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -31,7 +31,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: "TinyDolphin-tinydolphin.top",
+  utterancesGitHubRepo: "Zhuruohuan-tinydolphin.top",
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
